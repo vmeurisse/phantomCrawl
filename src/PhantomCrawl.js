1093ecfbd24009ed496ad0076ca1c326fe572ffa
Vincent Meurisse <dev@meurisse.org>
2013-06-01 14:15:09 +0200

    WIP #9: restart crashed threads

--- src/PhantomCrawl.js
+++ src/PhantomCrawl.js
@@ -9,6 +9,7 @@ var urlStore = require('./url/urlStore');
 var urlToPath = require('./url/urlToPath');
 
 var PhantomCrawl = function(config) {
+	this.config = config;
 	urlToPath.setBase(config.base || 'extract');
 	
 	urlStore.setFilters(config.urlFilters);
@@ -23,24 +24,36 @@ var PhantomCrawl = function(config) {
 	
 	if (config.maxDepth) require('./url/filters/level').setMaxLevel(config.maxDepth);
 		
-	var userAgent = config.userAgent || 'Mozilla/5.0 (PhantomCrawl/' + require('../package.json').version + '; bot) AppleWebKit/534.34(KHTML, like Gecko) Chrome/13.0.764.0';
+	config.userAgent = config.userAgent || 'Mozilla/5.0 (PhantomCrawl/' + require('../package.json').version + '; bot) AppleWebKit/534.34(KHTML, like Gecko) Chrome/13.0.764.0';
 	this.threads = [];
 	var nbThreads = config.nbThreads || 1;
 	while (nbThreads--) {
-		var thread = new CrawlerThread({
-			nbCrawlers: config.crawlerPerThread,
-			userAgent: userAgent
-		});
-		thread.on('idle', this.checkFinish.bind(this));
-		this.threads.push(thread);
+		this.startThread();
 	}
 	var rc = new RessourceCrawler({
-		userAgent: userAgent
+		userAgent: config.userAgent
 	});
 	rc.on('idle', this.checkFinish.bind(this));
 	this.threads.push(rc);
 };
 
+PhantomCrawl.prototype.startThread = function() {
+	var thread = new CrawlerThread({
+		nbCrawlers: this.config.crawlerPerThread,
+		userAgent: this.config.userAgent
+	});
+	thread.on('idle', this.checkFinish.bind(this));
+	thread.on('crash', this.threadCrash.bind(this, thread));
+	this.threads.push(thread);
+	
+};
+
+PhantomCrawl.prototype.threadCrash = function(thread) {
+	this.threads = this.threads.filter(function(t) {
+		return t !== thread;
+	});
+	this.startThread();
+};
 
 PhantomCrawl.prototype.checkFinish = function() {
 	for (var i = 0; i < this.threads.length; i++) {
@@ -48,6 +61,7 @@ PhantomCrawl.prototype.checkFinish = function() {
 	}
 	if (!urlStore.isEmpty()) return;
 	
+	console.log('Crawl done. Exiting');
 	for (i = 0; i < this.threads.length; i++) {
 		this.threads[i].exit();
 	}
